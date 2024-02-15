90b0cac48d5e75aa876adc4caa25daca5af52ebb
sabrina <38015100+sabrinatitem@users.noreply.github.com>
2024-02-15 21:41:49 +0100

    add color var and bash script

---
+++ src/colors.sh
@@ -0,0 +1,4 @@
+# colors.sh
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
---
+++ src/print_colors.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Source the color variables
+source colors.sh
+
+# Print messages in different colors
+echo -e "${RED}This is some red text"
+echo -e "${GREEN}And this is some green text"
