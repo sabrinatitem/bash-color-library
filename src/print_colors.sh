5809b29cd70dc1e1ad999a81b261be7a58d90b25
inasbou <inesbouaiss@gmail.com>
2024-02-17 19:10:52 +0100

    feat: afficher  les nombres par leurs couleurs

--- src/print_colors.sh
+++ src/print_colors.sh
@@ -6,3 +6,19 @@ source colors.sh
 # Print messages in different colors
 echo -e "${RED}This is some red text"
 echo -e "${GREEN}And this is some green text"
+
+
+# Using a for loop to print  colors 
+afficherCouluers(){
+for ((i = 0; i <= 255; i++)); do
+  
+    printf   "\e[38;5;${i}m%5d\e[0m"  "$i"     
+     echo -ne "\033[0m"
+    # echo -n "     " 
+        if [ $((i % 16)) -eq 0 ]; then
+           echo -e 
+        fi
+done
+}    
+
+afficherCouluers
